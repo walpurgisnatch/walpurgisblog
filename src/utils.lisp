34937b33d28a5a5f7185c0734f7f5b68f3148535
Walpurgisnatch <Kawemirs@gmail.com>
2021-09-07 17:28:36 +0700

    update, find-where

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,7 +1,13 @@
 (in-package :cl-user)
 (defpackage :walpurgisblog.utils
-  (:use :cl)
-  (:export :from-token))
+  (:use :cl
+        :sxql
+        :cl-annot.class
+        :datafly)
+  (:export :from-token
+           :key
+           :wildcard
+           :model-update))
 
 (in-package :walpurgisblog.utils)
 
@@ -12,4 +18,27 @@
 
 (defun key (arg)
   (intern (string arg) :keyword))
+
+(defun wildcard (string)
+  (concatenate 'string "%" string "%"))
   
+;;;; CRUD
+
+(defmacro model-update (table where &rest cols)
+  "update users (:id 1) :name john :sname doe"
+  `(execute
+    (update ,(key table)
+        (set= ,@(loop for col in cols collect col)
+         :updated_at (local-time:now))
+        (where (:= ,@where)))))
+
+(defmacro find-where (table where)
+  (let ((data (gensym)))
+    `(let ((,data (retrieve-all
+                   (select :*
+                     (from ,(key table))
+                     (where ,where)
+                     :as ',table))))
+       (if (and (consp ,data) (null (cdr ,data)))
+           (car ,data)
+           ,data))))
